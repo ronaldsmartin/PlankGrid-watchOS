8684dbd1eaef2e81cd294f3e264e5a1d257d2cb9
Ronald Martin <ronald@plangrid.com>
2018-07-21 19:49:05 -0700

    Prototype UI

--- PlankGrid WatchKit Extension/InterfaceController.swift
+++ PlankGrid WatchKit Extension/InterfaceController.swift
@@ -10,7 +10,17 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+final class InterfaceController: WKInterfaceController {
+
+    // MARK: Outlets
+
+    @IBOutlet private var titleLabel: WKInterfaceLabel!
+    @IBOutlet var progressGroup: WKInterfaceGroup!
+    @IBOutlet private var countdownScene: WKInterfaceSKScene!
+    @IBOutlet private var timerLabel: WKInterfaceTimer!
+    @IBOutlet private var progressLabel: WKInterfaceLabel!
+
+    // MARK: - Lifecycle
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -26,6 +36,14 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+    // MARK: - Actions
+
+    @IBAction private func toggleRunning(_ sender: Any) {
+    }
+
+    @IBAction private func reset(_ sender: Any) {
     }
 
 }
